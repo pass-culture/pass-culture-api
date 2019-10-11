0791315370062cd571643ecc38a987e274ae4a08
Anne-Marie <annemariee@free.fr>
2019-10-11 16:14:23 +0200

    Revert "(PC-2363) - add index in value property for better search, fix backref and change test"
    This reverts commit 22648ab506c86e0248dec0a60676a05935f561b1.

--- models/api_key.py
+++ models/api_key.py
@@ -6,14 +6,11 @@ from sqlalchemy import BigInteger, Column, CHAR, ForeignKey
 from models.db import Model
 from models.pc_object import PcObject
 
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 class ApiKey(PcObject, Model):
-    value = Column(CHAR(64),
-                   index=True,
-                   nullable=False
-                   )
+    value = Column(CHAR(64), nullable=False)
 
     offererId = Column(BigInteger,
                        ForeignKey('offerer.id'),
@@ -22,4 +19,5 @@ class ApiKey(PcObject, Model):
 
     offerer = relationship('Offerer',
                            foreign_keys=[offererId],
-                           backref=backref('apiKey', uselist=False))
+                           backref='apiKey',
+                           uselist=False)
