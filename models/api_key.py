5be9561d13c9d0ad6240d90d2629b12d29ffe991
Anne-Marie <annemariee@free.fr>
2019-10-11 16:14:23 +0200

    Revert "(PC-2363) - make offererId foreign_keys not nullable"
    This reverts commit a9a3e101c7669ed3da210fb21f06400e232ea7c3.

--- models/api_key.py
+++ models/api_key.py
@@ -10,7 +10,6 @@ from sqlalchemy.orm import relationship, backref
 
 
 class ApiKey(PcObject, Model):
-    # child
     value = Column(CHAR(64),
                    index=True,
                    nullable=False
@@ -19,7 +18,7 @@ class ApiKey(PcObject, Model):
     offererId = Column(BigInteger,
                        ForeignKey('offerer.id'),
                        index=True,
-                       nullable=False)
+                       nullable=True)
 
     offerer = relationship('Offerer',
                            foreign_keys=[offererId],
