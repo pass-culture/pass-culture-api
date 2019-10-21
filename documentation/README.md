c9c2024e24db8a1acd29df2bddc14a1a9265341a
Anne-Marie <anne-marie.esteves@beta.gouv.fr>
2019-10-21 18:21:13 +0200

    (PC-2364) first implementation of api doc with swagger, needs to be enhanced with authorization and good texts

---
+++ documentation/README.md
@@ -0,0 +1,12 @@
+## API SWAGGER DOCUMENTATION ##
+
+https://github.com/rochacbruno/flasgger
+
+
+Nous utilisons le standart https://swagger.io/specification/
+
+Lancer la doc `python api_doc.py`
+Ça tourne sur http://localhost:5000/documentation/swagger/
+
+
+https://github.com/flasgger/flasgger/blob/master/examples/example_app.py
---
+++ documentation/api_doc.py
@@ -0,0 +1,40 @@
+from flask import Flask, json, jsonify, request
+from flasgger import Swagger
+from http import HTTPStatus
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "version": "0.0.2",
+            "title": "Api v2",
+            "endpoint": 'v2_spec',
+            "description": 'This is the version 2 of our API',
+            "route": '/v2/bookings/token/',
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "specs_route": "/documentation/swagger/",
+    #'openapi': '3.0.1';
+    "swagger": '2.0'
+}
+
+
+app = Flask(__name__)
+
+app.config["SWAGGER"] = {
+    "title": "DOCUMENTATION API",
+    "uiversion": 3,
+}
+
+swag = Swagger(app, template_file='bookings_token_specifications_v2.json', config=swagger_config)
+
+@app.route('/v2/bookings/token/<token>', methods=['GET'])
+def get_bookings_token_by_token():
+    return jsonify(request.json), HTTPStatus.OK
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
