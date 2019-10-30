510f4e0815d5d90826c850626f7f4472df012257
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-10-30 18:58:32 +0100

    (PC-2433) add install data script for initializing postgresql db for metabase

--- scripts/install_data.py
+++ scripts/install_data.py
@@ -1,5 +1,5 @@
 from local_providers.install import install_local_providers
-from models.install import install_models
+from models.install import install_models, install_database_extensions
 from flask import current_app as app
 
 from utils.tutorials import upsert_tuto_mediations
@@ -12,4 +12,12 @@ def install_data():
         install_models()
         upsert_tuto_mediations()
         install_local_providers()
+    logger.info("Models and LocalProviders installed")
+
+
+@app.manager.command
+def install_data_for_metabase():
+    with app.app_context():
+        install_database_extensions()
+        install_models()
     logger.info("Models and LocalProviders installed")
