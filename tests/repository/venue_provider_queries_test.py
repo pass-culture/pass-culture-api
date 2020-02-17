4eb6a3ab32919b5a2d29b5f0e8034091e010d1ef
Daniel Baudry <dbaudry@octo.com>
2020-02-17 19:25:28 +0100

    (PC-2760) : Refacto venue provider sync function

--- tests/repository/venue_provider_queries_test.py
+++ tests/repository/venue_provider_queries_test.py
@@ -1,5 +1,6 @@
 from repository import repository
-from repository.venue_provider_queries import get_venue_providers_to_sync, get_nb_containers_at_work
+from repository.venue_provider_queries import get_venue_providers_to_sync, get_nb_containers_at_work, \
+    get_actives_venue_providers_for_specific_provider
 from tests.conftest import clean_database
 from tests.model_creators.generic_creators import create_offerer, create_venue, create_venue_provider
 from tests.model_creators.provider_creators import activate_provider
@@ -41,17 +42,34 @@ class GetVenueProvidersToSyncTest:
         # Then
         assert venue_providers == [venue_provider_1]
 
+    @clean_database
+    def test_should_return_only_active_venue_provider(self, app):
+        # Given
+        offerer = create_offerer()
+        venue_1 = create_venue(offerer, siret='12345678901234')
+        venue_2 = create_venue(offerer)
+        titelive_provider = activate_provider('TiteLiveStocks')
+        venue_provider_1 = create_venue_provider(venue_1, titelive_provider, is_active=True, sync_worker_id=None)
+        venue_provider_2 = create_venue_provider(venue_2, titelive_provider, is_active=False, sync_worker_id=None)
+        repository.save(venue_provider_1, venue_provider_2)
+
+        # When
+        venue_providers = get_venue_providers_to_sync(titelive_provider.id)
+
+        # Then
+        assert venue_providers == [venue_provider_1]
+
 
 class GetNbContainersAtWorkTest:
     @clean_database
     def test_should_return_number_of_venue_provider_with_worker_id(self, app):
         # Given
         offerer = create_offerer()
-        venue_1 = create_venue(offerer, siret='12345678901234')
-        venue_2 = create_venue(offerer)
+        venue1 = create_venue(offerer, siret='12345678901234')
+        venue2 = create_venue(offerer)
         titelive_provider = activate_provider('TiteLiveStocks')
-        venue_provider_1 = create_venue_provider(venue_1, titelive_provider, )
-        venue_provider_2 = create_venue_provider(venue_2, titelive_provider, sync_worker_id='1234567')
+        venue_provider_1 = create_venue_provider(venue1, titelive_provider)
+        venue_provider_2 = create_venue_provider(venue2, titelive_provider, sync_worker_id='1234567')
         repository.save(venue_provider_1, venue_provider_2)
 
         # When
@@ -59,3 +77,40 @@ class GetNbContainersAtWorkTest:
 
         # Then
         assert nb_containers_at_work == 1
+
+
+class GetActivesVenueProvidersForSpecificProviderTest:
+    @clean_database
+    def test_should_return_all_venue_provider_matching_provider_id(self, app):
+        # Given
+        offerer = create_offerer()
+        venue1 = create_venue(offerer, siret='12345678901234')
+        venue2 = create_venue(offerer)
+        titelive_provider = activate_provider('TiteLiveStocks')
+        allocine_provider = activate_provider('AllocineStocks')
+        venue_provider1 = create_venue_provider(venue1, titelive_provider)
+        venue_provider2 = create_venue_provider(venue2, allocine_provider)
+        repository.save(venue_provider1, venue_provider2)
+
+        # When
+        venue_providers = get_actives_venue_providers_for_specific_provider(titelive_provider.id)
+
+        # Then
+        assert venue_providers == [venue_provider1]
+
+    @clean_database
+    def test_should_return_all_actives_venue_provider_matching_provider_id(self, app):
+        # Given
+        offerer = create_offerer()
+        venue1 = create_venue(offerer, siret='12345678901234')
+        venue2 = create_venue(offerer)
+        titelive_provider = activate_provider('TiteLiveStocks')
+        venue_provider1 = create_venue_provider(venue1, titelive_provider)
+        venue_provider2 = create_venue_provider(venue2, titelive_provider, is_active=False)
+        repository.save(venue_provider1, venue_provider2)
+
+        # When
+        venue_providers = get_actives_venue_providers_for_specific_provider(titelive_provider.id)
+
+        # Then
+        assert venue_providers == [venue_provider1]
