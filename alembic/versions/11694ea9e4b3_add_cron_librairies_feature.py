9f8072d38270ad7db73cc09861eae596b411d9bb
Daniel Baudry <dbaudry@octo.com>
2020-02-17 19:09:36 +0100

    (PC-2760) : Add new feature for cron

---
+++ alembic/versions/11694ea9e4b3_add_cron_librairies_feature.py
@@ -0,0 +1,70 @@
+"""add_cron_librairies_feature
+
+Revision ID: 11694ea9e4b3
+Revises: 771cab29d46e
+Create Date: 2020-02-17 17:59:25.065850
+
+"""
+from enum import Enum
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '11694ea9e4b3'
+down_revision = '771cab29d46e'
+branch_labels = None
+depends_on = None
+
+
+class FeatureToggle(Enum):
+    SYNCRONIZE_LIBRAIRIES = 'Permettre la synchronisation journalière avec Librairies.fr'
+
+
+def upgrade():
+    new_values = ('WEBAPP_SIGNUP', 'DEGRESSIVE_REIMBURSEMENT_RATE', 'QR_CODE',
+                  'FULL_OFFERS_SEARCH_WITH_OFFERER_AND_VENUE', 'SEARCH_ALGOLIA', 'SEARCH_LEGACY',
+                  'SYNCRONIZE_LIBRAIRIES')
+    previous_values = ('WEBAPP_SIGNUP', 'DEGRESSIVE_REIMBURSEMENT_RATE', 'DUO_OFFER', 'QR_CODE',
+                       'FULL_OFFERS_SEARCH_WITH_OFFERER_AND_VENUE', 'SEARCH_ALGOLIA', 'SEARCH_LEGACY')
+
+    previous_enum = sa.Enum(*previous_values, name='featuretoggle')
+    new_enum = sa.Enum(*new_values, name='featuretoggle')
+    temporary_enum = sa.Enum(*previous_values, name='tmp_featuretoggle')
+
+    temporary_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE TMP_FEATURETOGGLE'
+               ' USING name::TEXT::TMP_FEATURETOGGLE')
+    previous_enum.drop(op.get_bind(), checkfirst=False)
+    new_enum.create(op.get_bind(), checkfirst=False)
+
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE FEATURETOGGLE'
+               ' USING name::TEXT::FEATURETOGGLE')
+    op.execute("""
+                INSERT INTO feature (name, description, "isActive")
+                VALUES ('%s', '%s', FALSE);
+                """ % (FeatureToggle.SYNCRONIZE_LIBRAIRIES.name, FeatureToggle.SYNCRONIZE_LIBRAIRIES.value))
+    temporary_enum.drop(op.get_bind(), checkfirst=False)
+
+
+def downgrade():
+    new_values = ('WEBAPP_SIGNUP', 'DEGRESSIVE_REIMBURSEMENT_RATE', 'DUO_OFFER', 'QR_CODE',
+                  'FULL_OFFERS_SEARCH_WITH_OFFERER_AND_VENUE', 'SEARCH_ALGOLIA', 'SEARCH_LEGACY')
+    previous_values = ('WEBAPP_SIGNUP', 'DEGRESSIVE_REIMBURSEMENT_RATE', 'QR_CODE',
+                       'FULL_OFFERS_SEARCH_WITH_OFFERER_AND_VENUE', 'SEARCH_ALGOLIA', 'SEARCH_LEGACY',
+                       'SYNCRONIZE_LIBRAIRIES')
+
+    previous_enum = sa.Enum(*previous_values, name='featuretoggle')
+    new_enum = sa.Enum(*new_values, name='featuretoggle')
+    temporary_enum = sa.Enum(*new_values, name='tmp_featuretoggle')
+
+    op.execute("DELETE FROM feature WHERE name = 'SYNCRONIZE_LIBRAIRIES'")
+    temporary_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE TMP_FEATURETOGGLE'
+               ' USING name::TEXT::TMP_FEATURETOGGLE')
+    previous_enum.drop(op.get_bind(), checkfirst=False)
+    new_enum.create(op.get_bind(), checkfirst=False)
+
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE FEATURETOGGLE'
+               ' USING name::TEXT::FEATURETOGGLE')
+    temporary_enum.drop(op.get_bind(), checkfirst=False)
