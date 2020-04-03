68c10cc64926650218e26a06b071bcdc9606959d
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-03 18:24:59 +0200

    (MOB): Remove test over sandbox

--- tests/sandboxes/industrial_test.py
+++ tests/sandboxes/industrial_test.py
@@ -9,58 +9,6 @@ from tests.sandboxes.mock_adresse_api import MOCK_ADRESSE_API_RESPONSE
 from utils.logger import logger
 
 
-@patch('sandboxes.scripts.utils.ban.requests.get')
-@clean_database
-def test_save_industrial_sandbox(mock_request, app):
-    # given
-    response_return_value = MagicMock(status_code=200, text='')
-    response_return_value.json = MagicMock(side_effect=MOCK_ADRESSE_API_RESPONSE)
-    mock_request.return_value = response_return_value
-
-    save_counts()
-    logger_info = logger.info
-    logger.info = lambda o: None
-
-    # when
-    save_sandbox('industrial')
-
-    # then
-    assert_created_counts(
-        ApiKey=13,
-        Booking=42,
-        Deposit=8,
-        Mediation=86,
-        Offer=106,
-        Offerer=14,
-        Product=155,
-        Recommendation=100,
-        Stock=102,
-        User=53,
-        UserOfferer=125,
-        Venue=22,
-    )
-
-    # teardown
-    logger.info = logger_info
-
-
-@patch('sandboxes.scripts.utils.ban.requests.get')
-@clean_database
-def test_all_helpers_are_returning_actual_sandbox_data(mock_request, app):
-    # given
-    response_return_value = MagicMock(status_code=200, text='')
-    response_return_value.json = MagicMock(side_effect=MOCK_ADRESSE_API_RESPONSE)
-    mock_request.return_value = response_return_value
-    save_sandbox('industrial')
-
-    # when
-    helpers = get_all_getters()
-
-    # then
-    for k, v in helpers.items():
-        assert v is not None, f"Getter '{k}' is not returning anything"
-
-
 class AlgoliaIndexingTest:
     @patch.dict('os.environ', {'ALGOLIA_TRIGGER_INDEXATION': '0'})
     @patch('sandboxes.scripts.creators.industrial.create_industrial_algolia_objects.clear_index')
