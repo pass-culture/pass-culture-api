8d18039b1a0453a1569db7f6d39471240acd2309
Daniel Baudry <dbaudry@octo.com>
2020-09-16 17:31:40 +0200

    Add new StockProvider domain

--- domain/libraires.py
+++ domain/libraires.py
@@ -1,36 +1,8 @@
-from datetime import datetime
 from typing import Callable
 
-from connectors.api_libraires import ApiLibrairesStocks, ApiHttpLibrairesStocks
-
+from connectors.api_libraires import ApiHttpLibrairesStocks
 
 LIBRAIRES_STOCK_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-
-
-# domain
-class ApiProvider(ABC):
-    @abstractmethod
-    def get_stock_information(self, siret: str, last_processed_isbn: str = '', modified_since: str = ''):
-        pass
-
-    @abstractmethod
-    def can_be_synchronized(self, siret: str) -> bool:
-        pass
-
-
-# connectors == repostiories sql
-class ApiLibrairesStocks(ApiProvider):
-    def __init__(self):
-        self.name = 'Libraires'
-        self.api_url = API_URL = 'https://passculture.leslibraires.fr/stocks'
-        self.api = ApiHttpLibrairesStocks(api_url=API_URL, name='Libraires')
-
-    def get_stock_information(self, siret: str, last_processed_isbn: str = '', modified_since: str = ''):
-        result = self.api.get_stocks_from_local_provider_api(siret, ...)
-        return result['Stocks']
-
-    def can_be_synchronized_with_libraires(self, siret: str) -> bool:
-        return self.api.is_siret_registered(siret)
 
 
 def get_libraires_stock_information(siret: str, last_processed_isbn: str = '', modified_since: str = '',
@@ -42,7 +14,3 @@ def get_libraires_stock_information(siret: str, last_processed_isbn: str = '', m
 def can_be_synchronized_with_libraires(siret: str) -> bool:
     api = ApiHttpLibrairesStocks(api_url=API_URL, name='Libraires')
     return api.is_siret_registered(siret)
-
-
-def read_last_modified_date(date: datetime) -> str:
-    return datetime.strftime(date, LIBRAIRES_STOCK_DATETIME_FORMAT) if date else ''
