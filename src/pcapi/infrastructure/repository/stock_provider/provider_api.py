359deebfb97a045bb88f79d9845718e2b8b1ceac
Daniel Baudry <dbaudry@octo.com>
2020-11-12 16:26:00 +0100

    FIX Black

--- src/pcapi/infrastructure/repository/stock_provider/provider_api.py
+++ src/pcapi/infrastructure/repository/stock_provider/provider_api.py
@@ -11,27 +11,29 @@ REQUEST_TIMEOUT_FOR_PROVIDERS_IN_SECOND = 30
 
 
 class ProviderAPI:
-
     def __init__(self, api_url: str, name: str, authentication_token: str = None):
         self.api_url = api_url
         self.name = name
         self.authentication_token = authentication_token
 
-    def stocks(self, siret: str, last_processed_reference: str = '', modified_since: str = '',
-               limit: int = 1000) -> Dict:
+    def stocks(
+        self, siret: str, last_processed_reference: str = "", modified_since: str = "", limit: int = 1000
+    ) -> Dict:
         api_url = self._build_local_provider_url(siret)
         params = self._build_local_provider_params(last_processed_reference, modified_since, limit)
         headers = {}
 
         if self.authentication_token is not None:
-            headers = {'Authorization': f'Basic {self.authentication_token}'}
+            headers = {"Authorization": f"Basic {self.authentication_token}"}
 
-        response = requests.get(url=api_url, params=params, headers=headers,
-                                timeout=REQUEST_TIMEOUT_FOR_PROVIDERS_IN_SECOND)
+        response = requests.get(
+            url=api_url, params=params, headers=headers, timeout=REQUEST_TIMEOUT_FOR_PROVIDERS_IN_SECOND
+        )
 
         if response.status_code != 200:
             raise ProviderAPIException(
-                f'Error {response.status_code} when getting {self.name} stocks for SIRET: {siret}')
+                f"Error {response.status_code} when getting {self.name} stocks for SIRET: {siret}"
+            )
 
         try:
             return response.json()
@@ -43,21 +45,21 @@ class ProviderAPI:
         headers = {}
 
         if self.authentication_token is not None:
-            headers = {'Authorization': f'Basic {self.authentication_token}'}
+            headers = {"Authorization": f"Basic {self.authentication_token}"}
 
         response = requests.get(url=api_url, headers=headers, timeout=REQUEST_TIMEOUT_FOR_PROVIDERS_IN_SECOND)
 
         return response.status_code == 200
 
     def _build_local_provider_url(self, siret: str) -> str:
-        return f'{self.api_url}/{siret}'
+        return f"{self.api_url}/{siret}"
 
     def _build_local_provider_params(self, last_processed_isbn: str, modified_since: str, limit: int) -> Dict:
-        params = {'limit': str(limit)}
+        params = {"limit": str(limit)}
 
         if last_processed_isbn:
-            params['after'] = last_processed_isbn
+            params["after"] = last_processed_isbn
         if modified_since:
-            params['modifiedSince'] = modified_since
+            params["modifiedSince"] = modified_since
 
         return params
