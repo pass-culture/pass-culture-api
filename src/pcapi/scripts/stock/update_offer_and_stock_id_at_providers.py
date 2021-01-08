5c42ccffe223dbc7e863f84579d4ce1a0c2981e2
Daniel Baudry <dbaudry@octo.com>
2021-01-08 17:08:12 +0100

    [PC-5599] Fix update offers siret script
      When a user change venue.siret, existing synchronized offer keep the
    old siret.
    this can create duplicated offers (one with old siret and one with the
    new one) or unsynchronized offers (idAtProvider doesn't match)
    
     Here we try to fix these two cases, for duplicated offers, we need to
    transfer existing bookings.
    When it's not possible, offers with errors aren't saved and "a solution
    must be found..."
    
     always start by executing a lazy run!

--- src/pcapi/scripts/stock/update_offer_and_stock_id_at_providers.py
+++ src/pcapi/scripts/stock/update_offer_and_stock_id_at_providers.py
@@ -1,50 +1,163 @@
+from collections import defaultdict
 from typing import List
 
 from sqlalchemy.orm import joinedload
 
+from pcapi.core.bookings.models import Booking
 from pcapi.core.offers.models import Offer
+from pcapi.core.offers.models import Stock
 from pcapi.models import VenueSQLEntity
 from pcapi.repository import repository
+from pcapi.utils.logger import logger
 
 
 BATCH_SIZE = 100
 
 
-def update_offer_and_stock_id_at_providers(venue_id: int) -> None:
+class UpdateOfferSiretException(Exception):
+    pass
+
+
+def update_offer_and_stock_id_at_providers(venue_id: int, old_siret: str, lazy_run: bool = True) -> None:
+    """We are looking for two things:
+    1) linked offers with the wrong siret. Indeed, when the siret of a venue is changed, linked offers need to be updated
+    2) linked offers that have been duplicated by the synchronization process. We want to :
+        * deactivate them
+        * transfer bookings to the offer with the right siret.
+    """
+
     venue = VenueSQLEntity.query.get(venue_id)
     current_siret = venue.siret
 
-    titelive_offers_to_update = _get_titelive_offers_with_old_id_at_providers(venue, current_siret)
+    titelive_offers_with_old_siret = _get_titelive_offers_with_siret_in_id_at_providers(venue, old_siret)
+    titelive_offers_with_current_siret = _get_titelive_offers_with_siret_in_id_at_providers(venue, current_siret)
+    offers_by_id_at_providers = {offer.idAtProviders: offer for offer in titelive_offers_with_current_siret}
 
     offers_to_update = []
     stocks_to_update = []
+    bookings_to_update = []
+    errors = defaultdict(list)
+    nb_offers_to_update = len(titelive_offers_with_old_siret)
+    nb_updated_offers = 0
 
-    for offer_to_update in titelive_offers_to_update:
-        offer_to_update.idAtProviders = _correct_id_at_providers(offer_to_update.idAtProviders, current_siret)
-        for stock_to_update in offer_to_update.stocks:
-            stock_to_update.idAtProviders = _correct_id_at_providers(stock_to_update.idAtProviders, current_siret)
-            stocks_to_update.append(stock_to_update)
+    logger.info("%s offers have to be updated", nb_offers_to_update)
 
-        offers_to_update.append(offer_to_update)
+    for index, offer_to_update in enumerate(titelive_offers_with_old_siret):
+        newIdAtProviders = _update_id_at_provider_siret(offer_to_update.idAtProviders, current_siret)
 
-        if len(offers_to_update) >= BATCH_SIZE:
-            repository.save(*offers_to_update)
-            repository.save(*stocks_to_update)
-            offers_to_update = []
-            stocks_to_update = []
+        # offers have been duplicated for old and current siret.
+        # we want to deactivate offers with old siret.
+        if newIdAtProviders in offers_by_id_at_providers.keys():
+            try:
+                valid_offer = offers_by_id_at_providers[newIdAtProviders]
+                offer, stocks, bookings = _handle_duplicated_offer(offer_to_update, valid_offer)
+                offers_to_update.append(offer)
+                if stocks:
+                    stocks_to_update.append(*stocks)
+                if bookings:
+                    bookings_to_update.append(*bookings)
 
-    repository.save(*offers_to_update)
-    repository.save(*stocks_to_update)
+            except UpdateOfferSiretException as e:
+                errors[offer_to_update.id].append(e)
+                continue
+
+        # offers and stock have a wrong idAtProviders.
+        else:
+            offer_to_update.idAtProviders = newIdAtProviders
+            offers_to_update.append(offer_to_update)
+            for stock_to_update in offer_to_update.stocks:
+                stock_to_update.idAtProviders = newIdAtProviders
+                stocks_to_update.append(stock_to_update)
+
+        if not lazy_run:
+            nb_offers_to_update = len(offers_to_update)
+
+            if nb_offers_to_update >= BATCH_SIZE or (
+                index == (nb_offers_to_update - 1) and (index % BATCH_SIZE) > 0 or nb_offers_to_update < BATCH_SIZE
+            ):
+                repository.save(*offers_to_update)
+                repository.save(*stocks_to_update)
+                repository.save(*bookings_to_update)
+
+                nb_updated_offers += len(offers_to_update)
+                offers_to_update = []
+                stocks_to_update = []
+                bookings_to_update = []
+
+    if lazy_run:
+        logger.info("Lazy run, nothing is saved")
+
+    logger.info("%s offers have been updated", nb_updated_offers)
+    logger.info("%s offers have encounter some errors", len(errors))
+    print("Errors details: ", errors)
 
 
-def _get_titelive_offers_with_old_id_at_providers(venue: VenueSQLEntity, current_siret: str) -> List[Offer]:
+def _handle_duplicated_offer(duplicated_offer: Offer, offer: Offer):
+    stocks = []
+    bookings = []
+
+    duplicated_offer.isActive = False
+    for stock_to_update in duplicated_offer.stocks:
+        stock_to_update.isSoftDeleted = True
+        stocks.append(stock_to_update)
+        bookings_to_update = _transfer_bookings(duplicated_offer, stock_to_update, offer)
+        if bookings_to_update:
+            bookings.append(*bookings_to_update)
+    return duplicated_offer, stocks, bookings
+
+
+def _transfer_bookings(duplicated_offer: Offer, old_stock: Stock, offer: Offer) -> List[Booking]:
+    bookings = []
+    # _get_transfer_booking_stock_id raise if anything's wrong
+    valid_stock = _get_transfer_booking_stock_id(
+        duplicated_offer,
+        offer.stocks,
+        duplicated_offer.id,
+        offer.id,
+    )
+
+    for booking in old_stock.bookings:
+        if valid_stock.remainingQuantity < old_stock.bookingsQuantity:
+            raise UpdateOfferSiretException(
+                "Unable to transfer bookings from duplicated offer (offerId=%s) to valid offer (offerId=%s). Not enough remaining quantity on the valid stock"
+                % (duplicated_offer.id, offer.id)
+            )
+        if booking.stockId != valid_stock.id:
+            booking.stockId = valid_stock.id
+            bookings.append(booking)
+
+    return bookings
+
+
+def _get_titelive_offers_with_siret_in_id_at_providers(venue: VenueSQLEntity, current_siret: str) -> List[Offer]:
     return (
         Offer.query.filter(Offer.venueId == venue.id)
-        .filter(~Offer.idAtProviders.endswith(current_siret))
+        .filter(Offer.idAtProviders.endswith(current_siret))
         .options(joinedload(Offer.stocks))
         .all()
     )
 
 
-def _correct_id_at_providers(id_at_providers: str, current_siret: str) -> str:
+def _update_id_at_provider_siret(id_at_providers: str, current_siret: str) -> str:
     return id_at_providers.split("@")[0] + "@" + current_siret
+
+
+def _get_transfer_booking_stock_id(old_stock: Stock, new_stocks: Stock, old_offer_id: str, new_offer_id: str) -> Stock:
+    error_params = (
+        old_offer_id,
+        new_offer_id,
+    )
+
+    nb_new_stocks = len(new_stocks)
+    if nb_new_stocks == 0:
+        raise UpdateOfferSiretException(
+            "Unable to transfer bookings from duplicated offer (offerId=%s) to valid offer (offerId=%s). No stock found on the valid offer."
+            % error_params
+        )
+    if nb_new_stocks > 1:
+        raise UpdateOfferSiretException(
+            "Unable to transfer bookings from duplicated offer (offerId=%s) to valid offer (offerId=%s). More than one stock on the valid offer."
+            % error_params
+        )
+
+    return new_stocks[0]
--- tests/scripts/stock/update_offer_and_stock_id_at_providers_test.py
+++ tests/scripts/stock/update_offer_and_stock_id_at_providers_test.py
@@ -3,12 +3,15 @@ from unittest.mock import patch
 
 import pytest
 
+from pcapi.core.bookings.factories import BookingFactory
 from pcapi.core.offers.factories import OfferFactory
 from pcapi.core.offers.factories import StockFactory
 from pcapi.core.offers.factories import VenueFactory
 from pcapi.repository import repository
-from pcapi.scripts.stock.update_offer_and_stock_id_at_providers import _correct_id_at_providers
-from pcapi.scripts.stock.update_offer_and_stock_id_at_providers import _get_titelive_offers_with_old_id_at_providers
+from pcapi.scripts.stock.update_offer_and_stock_id_at_providers import (
+    _get_titelive_offers_with_siret_in_id_at_providers,
+)
+from pcapi.scripts.stock.update_offer_and_stock_id_at_providers import _update_id_at_provider_siret
 from pcapi.scripts.stock.update_offer_and_stock_id_at_providers import update_offer_and_stock_id_at_providers
 
 
@@ -17,47 +20,169 @@ class UpdateOfferAndStockIdAtProvidersTest:
     @pytest.mark.usefixtures("db_session")
     def should_update_id_at_providers_for_offers_and_stocks_with_current_siret(self, mock_repository, app):
         # Given
+        old_siret = "85234081900014"
         current_siret = "32363560700019"
         venue = VenueFactory(siret=current_siret, id=12)
-        offer = OfferFactory(venue=venue, idAtProviders="9782742785988@85234081900014")
-        stock = StockFactory(offer=offer, idAtProviders="9782742785988@85234081900014")
+        offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{old_siret}")
+        stock = StockFactory(offer=offer, idAtProviders=f"9782742785988@{old_siret}")
+        repository.save(stock)
 
         # When
-        update_offer_and_stock_id_at_providers(venue_id=12)
+        update_offer_and_stock_id_at_providers(venue_id=venue.id, old_siret=old_siret, lazy_run=False)
 
         # Then
-        assert offer.idAtProviders == "9782742785988@32363560700019"
-        assert stock.idAtProviders == "9782742785988@32363560700019"
-        assert mock_repository.save.call_count == 2
-        assert mock_repository.save.call_args_list == [call(offer), call(stock)]
+        assert offer.idAtProviders == f"9782742785988@{current_siret}"
+        assert stock.idAtProviders == f"9782742785988@{current_siret}"
+        assert mock_repository.save.call_count == 3
+        assert mock_repository.save.call_args_list == [call(offer), call(stock), call()]
 
-    class GetTiteliveOffersWithOldIdAtProvidersTest:
+    @patch("pcapi.scripts.stock.update_offer_and_stock_id_at_providers.repository")
+    @pytest.mark.usefixtures("db_session")
+    def should_soft_delete_offer_and_stock_when_new_id_at_providers_offer_already_exist(self, mock_repository, app):
+        # Given
+        old_siret = "85234081900014"
+        current_siret = "32363560700019"
+        venue = VenueFactory(siret=current_siret, id=12)
+        offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{old_siret}")
+        new_offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{current_siret}")
+        stock = StockFactory(offer=offer, idAtProviders=f"9782742785988@{old_siret}")
+        new_stock = StockFactory(offer=new_offer, idAtProviders=f"9782742785988@{current_siret}")
+
+        repository.save(stock, new_stock)
+
+        # When
+        update_offer_and_stock_id_at_providers(venue_id=venue.id, old_siret=old_siret, lazy_run=False)
+
+        # Then
+        assert offer.isActive is False
+        assert new_offer.isActive is True
+        assert stock.isSoftDeleted is True
+        assert new_stock.isSoftDeleted is False
+        assert mock_repository.save.call_count == 3
+        assert mock_repository.save.call_args_list == [call(offer), call(stock), call()]
+
+    @patch("pcapi.scripts.stock.update_offer_and_stock_id_at_providers.repository")
+    @pytest.mark.usefixtures("db_session")
+    def should_keep_bookings_to_existing_stock(self, mock_repository, app):
+        # Given
+        old_siret = "85234081900014"
+        current_siret = "32363560700019"
+        venue = VenueFactory(siret=current_siret, id=12)
+        offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{old_siret}")
+        stock = StockFactory(id=11, offer=offer, idAtProviders=f"9782742785988@{old_siret}")
+        booking = BookingFactory(stock=stock)
+
+        repository.save(booking)
+
+        # When
+        update_offer_and_stock_id_at_providers(venue_id=12, old_siret=old_siret, lazy_run=False)
+
+        # Then
+        assert booking.stockId == 11
+        assert mock_repository.save.call_count == 3
+        assert mock_repository.save.call_args_list == [call(offer), call(stock), call()]
+
+    @patch("pcapi.scripts.stock.update_offer_and_stock_id_at_providers.repository")
+    @pytest.mark.usefixtures("db_session")
+    def should_switch_bookings_to_new_stock(self, mock_repository, app):
+        # Given
+        old_siret = "85234081900014"
+        current_siret = "32363560700019"
+        venue = VenueFactory(siret=current_siret, id=12)
+
+        old_offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{old_siret}")
+        old_stock = StockFactory(id=100, offer=old_offer, idAtProviders=f"9782742785988@{old_siret}")
+        booking = BookingFactory(stock=old_stock)
+
+        valid_offer = OfferFactory(venue=venue, idAtProviders=f"9782742785988@{current_siret}")
+        valid_stock = StockFactory(id=200, offer=valid_offer, idAtProviders=f"9782742785988@{current_siret}")
+
+        repository.save(old_stock, valid_stock, booking)
+
+        # When
+        update_offer_and_stock_id_at_providers(venue_id=venue.id, old_siret=old_siret, lazy_run=False)
+
+        # Then
+        assert booking.stockId == valid_stock.id
+        assert mock_repository.save.call_count == 3
+        assert mock_repository.save.call_args_list == [call(old_offer), call(old_stock), call(booking)]
+
+    @patch("pcapi.scripts.stock.update_offer_and_stock_id_at_providers.repository")
+    @pytest.mark.usefixtures("db_session")
+    def should_not_save_offer_with_bookings_errors(self, mock_repository, app):
+        # Given
+        old_siret = "85234081900014"
+        current_siret = "32363560700019"
+        venue = VenueFactory(siret=current_siret, id=12)
+
+        valid_offer_product_ids = "9782742785988"
+        not_valid_offer_product_ids = "1234567890987"
+        old_offers = {}
+        bookings = {}
+        stocks = {}
+        old_stocks = {}
+        for product_id in [valid_offer_product_ids, not_valid_offer_product_ids]:
+
+            stock_quantity = 1 if product_id == valid_offer_product_ids else 0
+            offer = OfferFactory(venue=venue, idAtProviders=f"{product_id}@{current_siret}")
+            stocks[product_id] = StockFactory(
+                offer=offer, quantity=stock_quantity, idAtProviders=f"{product_id}@{current_siret}"
+            )
+
+            old_offers[product_id] = OfferFactory(venue=venue, idAtProviders=f"{product_id}@{old_siret}")
+            old_stocks[product_id] = StockFactory(
+                offer=old_offers[product_id], idAtProviders=f"{product_id}@{old_siret}"
+            )
+            bookings[product_id] = BookingFactory(stock=old_stocks[product_id])
+
+            repository.save(old_stocks[product_id], stocks[product_id], bookings[product_id])
+
+        # When
+        update_offer_and_stock_id_at_providers(venue_id=venue.id, old_siret=old_siret, lazy_run=False)
+
+        # Then
+        assert bookings[valid_offer_product_ids].stockId == stocks[valid_offer_product_ids].id
+        assert bookings[not_valid_offer_product_ids].stockId == old_stocks[not_valid_offer_product_ids].id
+        assert mock_repository.save.call_count == 3
+        assert mock_repository.save.call_args_list == [
+            call(old_offers[valid_offer_product_ids]),
+            call(old_stocks[valid_offer_product_ids]),
+            call(bookings[valid_offer_product_ids]),
+        ]
+
+    class GetTiteliveOffersWithSiretInIdAtProvidersTest:
         @pytest.mark.usefixtures("db_session")
-        def should_return_offers_with_id_at_providers_including_different_siret(self, app):
+        def should_return_offers_with_siret_in_id_at_providers(self, app):
             # Given
-            current_siret = "32363560700019"
-            expected_venue = VenueFactory(siret=current_siret)
-            other_venue = VenueFactory(siret="7863560700657")
-            offer1 = OfferFactory(venue=expected_venue, idAtProviders="9782742785988@85234081900014")
-            offer2 = OfferFactory(venue=expected_venue, idAtProviders="9782742785988@32363560700019")
-            other_offer = OfferFactory(venue=other_venue, idAtProviders="9782742785988@7863560700657")
+            siret = "32363560700019"
+            old_siret = "85234081900014"
+            other_siret = "7863560700657"
 
-            repository.save(offer1, offer2, other_offer)
+            current_siret = "32363560700019"
+
+            expected_venue = VenueFactory(siret=siret)
+            other_venue = VenueFactory(siret=other_siret)
+            offer = OfferFactory(venue=expected_venue, idAtProviders=f"9782742785988@{expected_venue.siret}")
+            offer_with_old_siret = OfferFactory(venue=expected_venue, idAtProviders=f"9782742785988@{old_siret}")
+            other_offer = OfferFactory(venue=other_venue, idAtProviders=f"9782742785988@{other_venue.siret}")
+
+            repository.save(offer, offer_with_old_siret, other_offer)
 
             # When
-            offers_result = _get_titelive_offers_with_old_id_at_providers(expected_venue, current_siret)
+            offers_result = _get_titelive_offers_with_siret_in_id_at_providers(expected_venue, current_siret)
 
             # Then
-            assert offers_result == [offer1]
+            assert offers_result == [offer]
 
     class CorrectIdAtProvidersTest:
         def should_replace_siret_in_id_at_providers_with_given_value(self):
             # Given
-            current_id_at_providers = "9782742785988@85234081900014"
+            old_siret = "85234081900014"
             current_siret = "32363560700019"
+            current_id_at_providers = f"9782742785988@{old_siret}"
 
             # When
-            result = _correct_id_at_providers(current_id_at_providers, current_siret)
+            result = _update_id_at_provider_siret(current_id_at_providers, current_siret)
 
             # Then
-            assert result == "9782742785988@32363560700019"
+            assert result == f"9782742785988@{current_siret}"
