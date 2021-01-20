c5944834ba5d7a6b60c82c5d076efa811a04e404
Damien Baty <damien.baty@polyconseil.fr>
2021-01-20 23:23:49 +0100

    tests: Rewrite tests to avoid using `assert_created_counts()`
    `assert_created_counts()` expects to find all models from the
    `pcapi.models` module, which is not the case anymore. Let's remove it!
    
    Extra:
    
    1. I cleaned up `test_save_activation_sandbox`:
    
       - no need to deactivate the logger (especially like this without
       using try/finally to properly restore it);
    
       - no need to check that there is no UserOfferer nor Deposit, since
       we already check that there is no User. (I am not sure what is the
       purpose of these checks, though...)
    
    2. By the way, while you're here: if you know why the test is named
    `test_compute_first_thumb_dominant_color_even_if_not_first_file`,
    please entertain me, I'm curious. The notion of dominant color has
    long been removed from the code base and, as far as I can tell, the
    test has never tested anything related to a color.

--- tests/sandboxes/activation_test.py
+++ tests/sandboxes/activation_test.py
@@ -1,36 +1,27 @@
 import pytest
 
+from pcapi.core.bookings.models import Booking
+from pcapi.core.offers.models import Mediation
+from pcapi.core.offers.models import Offer
 from pcapi.core.offers.models import Stock
-from pcapi.model_creators.provider_creators import assert_created_counts
-from pcapi.model_creators.provider_creators import save_counts
+from pcapi.core.users.models import User
+from pcapi.models.offerer import Offerer
+from pcapi.models.product import Product
 from pcapi.sandboxes.scripts.save_sandbox import save_sandbox
-from pcapi.utils.logger import logger
 
 
 @pytest.mark.usefixtures("db_session")
 def test_save_activation_sandbox(app):
     # given
-    save_counts()
-    logger_info = logger.info
-    logger.info = lambda o: None
-
-    # when
     save_sandbox("activation")
 
     # then
-    assert_created_counts(
-        Booking=0,
-        Deposit=0,
-        Mediation=0,
-        Offer=1,
-        Offerer=1,
-        Product=1,
-        Stock=1,
-        User=0,
-        UserOfferer=0,
-    )
+    assert Booking.query.count() == 0
+    assert Mediation.query.count() == 0
+    assert Offer.query.count() == 1
+    assert Offerer.query.count() == 1
+    assert Product.query.count() == 1
+    assert Stock.query.count() == 1
+    assert User.query.count() == 0
 
-    assert Stock.query.first().quantity == 10000
-
-    # teardown
-    logger.info = logger_info
+    assert Stock.query.one().quantity == 10000
