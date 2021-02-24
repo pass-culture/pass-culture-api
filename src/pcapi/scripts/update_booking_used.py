58b5e3818d22419343b5ab9ae5e1aa04427cde54
cgaunet <cyrilg@theodo.fr>
2021-02-24 08:39:55 +0100

    (PC-XXX) Improve the way update booking used is working

--- src/pcapi/scripts/update_booking_used.py
+++ src/pcapi/scripts/update_booking_used.py
@@ -1,22 +1,40 @@
 from datetime import datetime
+from typing import List
 
+from pcapi.core.bookings.models import Booking
 import pcapi.core.bookings.repository as booking_repository
 from pcapi.repository import repository
+from pcapi.utils.logger import logger
 
 
-def update_booking_used_after_stock_occurrence():
+def update_booking_used_after_stock_occurrence() -> None:
     bookings_to_process = booking_repository.find_not_used_and_not_cancelled()
-    bookings_id_errors = []
+    logger.info("processing %d bookings to update", len(bookings_to_process))
+    bookings_to_update = []
+    bookings_id_errors: List[Booking] = []
 
-    for booking in bookings_to_process:
+    for booking_index, booking in enumerate(bookings_to_process):
         if booking.stock.beginningDatetime:
             now = datetime.utcnow()
             if not booking.stock.isEventDeletable:
                 booking.isUsed = True
                 booking.dateUsed = now
-                try:
-                    repository.save(booking)
-                except Exception:  # pylint: disable=broad-except
-                    bookings_id_errors.append(booking.id)
+                bookings_to_update.append(booking)
 
-    print("Bookings id in error %s" % bookings_id_errors)
+        if booking_index % 1000 == 0:
+            logger.info("processing %d page", booking_index / 1000)
+            logger.info("%d bookings to update", len(bookings_to_update))
+            _save_bookings_to_update_in_db(bookings_to_update, bookings_id_errors)
+            bookings_to_update = []
+
+    _save_bookings_to_update_in_db(bookings_to_update, bookings_id_errors)
+
+    if len(bookings_id_errors) > 0:
+        logger.exception("Bookings id in error %s", bookings_id_errors)
+
+
+def _save_bookings_to_update_in_db(bookings_to_update: List[Booking], bookings_id_errors: List[Booking]) -> None:
+    try:
+        repository.save(*bookings_to_update)
+    except Exception:  # pylint: disable=broad-except
+        bookings_id_errors.extend(bookings_to_update)
