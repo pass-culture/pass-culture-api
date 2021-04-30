58eae532ee782e06ef35f1f4be20ff521f060fa8
apibrac <alexis.pibrac@gmail.com>
2021-04-30 17:43:37 +0200

    Correct providerAPI test script

--- src/pcapi/scripts/provider/check_provider_api.py
+++ src/pcapi/scripts/provider/check_provider_api.py
@@ -20,10 +20,11 @@ def check_provider_api(url, siret, token):
     assert provider_api.is_siret_registered(siret), "L'appel avec uniquement le siret dans l'url doit fonctionner"
 
     stocks = provider_api.stocks(siret, limit=1)
-    assert stocks.get("total") != None, "Le total est manquant."
-    assert int(stocks.get("total")) > 0, "Le total n'est pas strictement supérieur à 0."
+    # assert stocks.get("total") != None, "Le total est manquant."
+    # assert int(stocks.get("total")) > 0, "Le total n'est pas strictement supérieur à 0."
 
     assert "stocks" in stocks, 'La clé "stocks" est manquante.'
+    assert len(stocks.get("stocks")) > 0, "Le nombre de stock est nul"
     stock = stocks["stocks"][0]
     assert isinstance(stock, dict), "Chaque stock devrait être un objet (dictionnaire)."
     assert "ref" in stock, 'Chaque stock devrait avoir une clé "ref".'
@@ -38,19 +39,22 @@ def check_provider_api(url, siret, token):
     assert (
         next_stock.get("stocks")[0]["ref"] == two_stocks.get("stocks")[1]["ref"]
     ), "Le after doit correctement décaller les résultats"
-    assert int(stocks.get("total")) == int(next_stock.get("total")), "Le after ne doit pas impacter le total"
+    # assert int(stocks.get("total")) == int(next_stock.get("total")), "Le after ne doit pas impacter le total"
 
     no_stocks = provider_api.stocks(
         siret, limit=1, modified_since=datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%SZ")
     )
-    modified_since_is_implemented = int(stocks.get("total")) > int(no_stocks.get("total"))
+    modified_since_is_implemented = len(stocks.get("stocks")) > len(no_stocks.get("stocks"))
 
     stock_count = 0
     batch_count = 0
     start_time = time.time()
-    for raw_stocks in synchronize_provider_api._get_stocks_by_batch(siret, provider_api, None):
+    for raw_stocks in synchronize_provider_api._get_stocks_by_batch(
+        siret, provider_api, None  # datetime.utcnow() - timedelta(days=1)
+    ):
         stock_count += len(raw_stocks)
         batch_count += 1
+        print(f"Iteration: {stock_count} stocks, {batch_count} batches")
     duration = time.time() - start_time
     average_duration = duration / batch_count
 
