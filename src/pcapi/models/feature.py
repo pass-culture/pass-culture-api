8a1437b98df018c14e9001264900c63bc212c047
apibrac <alexis.pibrac@gmail.com>
2021-05-09 16:01:53 +0200

    (PC-8776): Stop using PARALLEL_SYNCHRONIZATION_OF_VENUE_PROVIDER

--- src/pcapi/models/feature.py
+++ src/pcapi/models/feature.py
@@ -36,6 +36,7 @@ class FeatureToggle(enum.Enum):
     APPLY_BOOKING_LIMITS_V2 = "Permettre l affichage des nouvelles règles de génération de portefeuille des jeunes"
     ALLOW_IDCHECK_REGISTRATION = "Autoriser les utilisateurs à suivre le parcours d inscription ID Check"
     WHOLE_FRANCE_OPENING = "Ouvre le service à la France entière"
+    # FIXME (apibrac, 2021/05/10): Deprecated:
     PARALLEL_SYNCHRONIZATION_OF_VENUE_PROVIDER = (
         "Active la parallèlisation des opérations de synchronisation pour les VenueProvider"
     )
