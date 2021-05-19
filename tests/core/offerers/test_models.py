31929bc964765a3de06a01afa70b7690d3c30b5e
François Séguin <francois.seguin@polyconseil.fr>
2021-05-19 11:34:19 +0200

    (PC-7132) offerers: add nValidatedOffers property to Venue and Offerer
    (cherry picked from commit 2f4d0a00916873a2151f75d9afed64d0e2cb659b)

--- tests/core/offerers/test_models.py
+++ tests/core/offerers/test_models.py
@@ -2,24 +2,24 @@ import pytest
 
 from pcapi.core.offerers.models import Offerer
 from pcapi.core.offerers.models import Venue
-from pcapi.core.offers import factories
-import pcapi.core.offers.factories as offers_factories
+from pcapi.core.offers import factories as offers_factories
+from pcapi.core.offers.models import OfferValidationStatus
 
 
 @pytest.mark.usefixtures("db_session")
 class VenueTimezonePropertyTest:
     def test_europe_paris_is_default_timezone(self):
-        venue = factories.VenueFactory(postalCode="75000")
+        venue = offers_factories.VenueFactory(postalCode="75000")
 
         assert venue.timezone == "Europe/Paris"
 
     def test_return_timezone_given_venue_departement_code(self):
-        venue = factories.VenueFactory(postalCode="97300")
+        venue = offers_factories.VenueFactory(postalCode="97300")
 
         assert venue.timezone == "America/Cayenne"
 
     def test_return_managing_offerer_timezone_when_venue_is_virtual(self):
-        venue = factories.VirtualVenueFactory(managingOfferer__postalCode="97300")
+        venue = offers_factories.VirtualVenueFactory(managingOfferer__postalCode="97300")
 
         assert venue.timezone == "America/Cayenne"
 
@@ -27,21 +27,21 @@ class VenueTimezonePropertyTest:
 @pytest.mark.usefixtures("db_session")
 class VenueTimezoneSqlQueryTest:
     def test_europe_paris_is_default_timezone(self):
-        factories.VenueFactory(postalCode="75000")
+        offers_factories.VenueFactory(postalCode="75000")
 
         query_result = Venue.query.filter(Venue.timezone == "Europe/Paris").all()
 
         assert len(query_result) == 1
 
     def test_return_timezone_given_venue_departement_code(self):
-        factories.VenueFactory(postalCode="97300")
+        offers_factories.VenueFactory(postalCode="97300")
 
         query_result = Venue.query.filter(Venue.timezone == "America/Cayenne").all()
 
         assert len(query_result) == 1
 
     def test_return_managing_offerer_timezone_when_venue_is_virtual(self):
-        factories.VirtualVenueFactory(managingOfferer__postalCode="97300")
+        offers_factories.VirtualVenueFactory(managingOfferer__postalCode="97300")
 
         query_result = Venue.query.filter(Venue.timezone == "America/Cayenne").all()
 
@@ -76,3 +76,27 @@ class OffererDepartementCodeSQLExpressionTest:
         query_result = Offerer.query.filter(Offerer.departementCode == "973").all()
 
         assert len(query_result) == 1
+
+
+@pytest.mark.usefixtures("db_session")
+class OffererNValidatedOffersTest:
+    def test_offerer_with_validated_offers(self):
+        offerer = offers_factories.OffererFactory()
+        offers_factories.OfferFactory.create_batch(
+            size=2, validation=OfferValidationStatus.APPROVED, venue__managingOfferer=offerer
+        )
+
+        assert offerer.nApprovedOffers == 2
+
+    def test_offerer_without_offer(self):
+        offerer = offers_factories.OffererFactory()
+
+        assert offerer.nApprovedOffers == 0
+
+    def test_offerer_without_validated_offer(self):
+        offerer = offers_factories.OffererFactory()
+        offers_factories.OfferFactory(validation=OfferValidationStatus.DRAFT, venue__managingOfferer=offerer)
+        offers_factories.OfferFactory(validation=OfferValidationStatus.REJECTED, venue__managingOfferer=offerer)
+        offers_factories.OfferFactory(validation=OfferValidationStatus.PENDING, venue__managingOfferer=offerer)
+
+        assert offerer.nApprovedOffers == 0
