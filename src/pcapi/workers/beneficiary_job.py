8122848e84e2b94f05c13be1866cee3123a1eea6
Victor Lebrun <victor.lebrun011@gmail.com>
2021-07-15 15:13:36 +0200

    (PC-9977) Add a beneficiary_import mode to bypass idPieceNumber duplicate check
    For a good amount of uploaded identity cards, Jouve sends a wrong value for idPieceNumber field, for example ''. Some files have been manually checked by admins and must be reimported by bypassing the check

--- src/pcapi/workers/beneficiary_job.py
+++ src/pcapi/workers/beneficiary_job.py
@@ -9,5 +9,15 @@ from pcapi.workers.decorators import log_job
 @job(worker.id_check_queue, connection=worker.conn)
 @job_context
 @log_job
-def beneficiary_job(application_id: int, run_fraud_detection: bool = True, fraud_detection_ko: bool = False) -> None:
-    create_beneficiary_from_application.execute(application_id, run_fraud_detection, fraud_detection_ko)
+def beneficiary_job(
+    application_id: int,
+    run_fraud_detection: bool = True,
+    ignore_id_piece_number_field: bool = False,
+    fraud_detection_ko: bool = False,
+) -> None:
+    create_beneficiary_from_application.execute(
+        application_id,
+        run_fraud_detection=run_fraud_detection,
+        ignore_id_piece_number_field=ignore_id_piece_number_field,
+        fraud_detection_ko=fraud_detection_ko,
+    )
