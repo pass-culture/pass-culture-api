bddb2bf1d084263d7d91c68572e53b617728b139
François Séguin <francois.seguin@polyconseil.fr>
2021-09-07 12:03:25 +0200

    sandboxes: use generic pictures to make all subcategories thumbnails

--- src/pcapi/sandboxes/scripts/utils/storage_utils.py
+++ src/pcapi/sandboxes/scripts/utils/storage_utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from pcapi.connectors.thumb_storage import create_thumb
 from pcapi.core.object_storage import store_public_object
@@ -6,22 +7,17 @@ from pcapi.utils.human_ids import humanize
 from pcapi.utils.string_processing import get_model_plural_name
 
 
-def store_public_object_from_sandbox_assets(folder, model, offer_type):
+def store_public_object_from_sandbox_assets(folder, model, subcategoryId):
     mimes_by_folder = {"spreadsheets": "application/CSV", "thumbs": "image/jpeg", "zips": "application/zip"}
     plural_model_name = get_model_plural_name(model)
     thumb_id = humanize(model.id)
-    thumb_path = f"{os.path.dirname(os.path.realpath(__file__))}/../../{folder}/{plural_model_name}/{str(offer_type)}"
-
-    with open(thumb_path, mode="rb") as thumb_file:
+    thumbs_folder_path = Path(os.path.dirname(os.path.realpath(__file__))) / ".." / ".." / "thumbs"
+    picture_path = str(thumbs_folder_path / "mediations" / subcategoryId) + ".jpg"
+    with open(picture_path, mode="rb") as thumb_file:
         if folder == "thumbs":
             create_thumb(model, thumb_file.read(), 0)
             model.thumbCount += 1
         else:
-            store_public_object(
-                folder,
-                plural_model_name + "/" + thumb_id,
-                thumb_file.read(),
-                mimes_by_folder[folder],
-            )
+            store_public_object(folder, plural_model_name + "/" + thumb_id, thumb_file.read(), mimes_by_folder[folder])
 
     return model
