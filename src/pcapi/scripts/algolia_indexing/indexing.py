80af87f7edfea5bf665e44321def35ef4386302a
Jeremie Baudet <jeremie.baudet@passculture.app>
2021-10-14 16:05:00 +0200

    (PC) rename limit parameter to batch size
    With limit, one could think that it would mean the total value, and not
    the number of items per page.
    
    Eg. "I want to process 2500 offers (total), 500 by 500".
    
    Before.
    This meant running this command:
    process_offers_from_database --limit=500 --ending-page=5 [...]
    
    Which could be interpreted as: I want to process 500 offers (total),
    100 (500/5) by 100.
    
    After.
    With the batch-size option, the command will be:
    process_offers_from_database --batch-size=500 --ending-page=5 [...]
    
    Which is less confusing: "I want 5 batches of 500 offers".

--- src/pcapi/scripts/algolia_indexing/indexing.py
+++ src/pcapi/scripts/algolia_indexing/indexing.py
@@ -8,14 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 def batch_indexing_offers_in_algolia_from_database(
-    ending_page: int = None, limit: int = 10000, starting_page: int = 0
+    ending_page: int = None, batch_size: int = 10000, starting_page: int = 0
 ) -> None:
     page = starting_page
     while True:
         if ending_page and ending_page == page:
             break
 
-        offer_ids = offer_queries.get_paginated_active_offer_ids(limit=limit, page=page)
+        offer_ids = offer_queries.get_paginated_active_offer_ids(limit=batch_size, page=page)
         if not offer_ids:
             break
         search.reindex_offer_ids(offer_ids)
--- tests/scripts/algolia_indexing/indexing_test.py
+++ tests/scripts/algolia_indexing/indexing_test.py
@@ -14,7 +14,7 @@ class BatchIndexingOffersInAlgoliaFromDatabaseTest:
         mock_get_paginated_active_offer_ids.side_effect = [[1], []]
 
         # When
-        batch_indexing_offers_in_algolia_from_database(ending_page=None, limit=1, starting_page=0)
+        batch_indexing_offers_in_algolia_from_database(ending_page=None, batch_size=1, starting_page=0)
 
         # Then
         assert mock_get_paginated_active_offer_ids.call_count == 2
@@ -29,7 +29,7 @@ class BatchIndexingOffersInAlgoliaFromDatabaseTest:
         mock_get_paginated_active_offer_ids.side_effect = [[1], [2], []]
 
         # When
-        batch_indexing_offers_in_algolia_from_database(ending_page=None, limit=1, starting_page=0)
+        batch_indexing_offers_in_algolia_from_database(ending_page=None, batch_size=1, starting_page=0)
 
         # Then
         assert mock_get_paginated_active_offer_ids.call_count == 3
@@ -47,7 +47,7 @@ class BatchIndexingOffersInAlgoliaFromDatabaseTest:
         mock_get_paginated_active_offer_ids.side_effect = [[1], [2], []]
 
         # When
-        batch_indexing_offers_in_algolia_from_database(ending_page=1, limit=1, starting_page=0)
+        batch_indexing_offers_in_algolia_from_database(ending_page=1, batch_size=1, starting_page=0)
 
         # Then
         mock_reindex_offer_ids.assert_called_once_with([1])
