768a9577d8703104ea9c58c03284585cb7a0a735
thig-pc <thierry.gabin@passculture.app>
2021-11-04 10:43:12 +0100

    (PC-11410) tested request get_users_who_will_turn_eighteen_in_one_month

--- src/pcapi/core/users/external/user_automations.py
+++ src/pcapi/core/users/external/user_automations.py
@@ -1,17 +1,43 @@
 from datetime import datetime
+from typing import List
+from typing import Optional
 
 from dateutil.relativedelta import relativedelta
+from sqlalchemy import func
 
 from pcapi.core.users.constants import ELIGIBILITY_AGE_18
 from pcapi.models import User
 
 
 AGE18_ELIGIBLE_BIRTH_DATE = datetime.utcnow() - relativedelta(years=ELIGIBILITY_AGE_18)
-_18_YEARS_AGO = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(
-    years=ELIGIBILITY_AGE_18
-)
-_19_YEARS_AGO = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(years=19)
 
 
-def get_users_who_turned_eighteen_today():
-    return User.query.filter(User.dateOfBirth.between(_19_YEARS_AGO, _18_YEARS_AGO)).all()
+def get_users_who_will_turn_eighteen_in_one_month() -> Optional[List[User]]:
+    _18_YEARS_IN_1_MONTHS = (
+        datetime.utcnow() - relativedelta(years=ELIGIBILITY_AGE_18) + relativedelta(days=30)
+    ).date()
+    return User.query.filter(func.date(User.dateOfBirth) == _18_YEARS_IN_1_MONTHS).all()
+
+
+def get_users_beneficiary_three_months_before_credit_expiration() -> Optional[List[User]]:
+    # not tested yet
+    IN_3_MONTHS = (datetime.utcnow() + relativedelta(days=90)).date()
+    return User.query.filter(func.date(User.deposit_expiration_date) == IN_3_MONTHS).all()
+
+
+def get_inactive_user_since_thirty_days() -> Optional[List[User]]:
+    # not tested yet
+    _30_DAYS_AGO = (datetime.utcnow() - relativedelta(days=30)).date()
+    return User.query.filter(func.date(User.lastConnectionDate) >= _30_DAYS_AGO).all()
+
+
+def get_users_having_one_year_since_subscription_by_month() -> Optional[List[User]]:
+    # not tested yet
+    # example : get all users subscribed in january from one year ago
+    _12_MONTHS_AGO = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(days=365)
+    _11_MONTHS_AGO = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - relativedelta(days=335)
+    return (
+        User.query.filter(func.date(User.deposit_activation_date) >= _12_MONTHS_AGO)
+        .filter(func.date(User.deposit_activation_date) <= _11_MONTHS_AGO)
+        .all()
+    )
--- tests/core/users/external/user_automations_test.py
+++ tests/core/users/external/user_automations_test.py
@@ -5,26 +5,30 @@ from freezegun import freeze_time
 import pytest
 
 from pcapi.core.users.constants import ELIGIBILITY_AGE_18
-from pcapi.core.users.external.user_automations import get_users_who_turned_eighteen_today
+from pcapi.core.users.external.user_automations import get_users_who_will_turn_eighteen_in_one_month
 import pcapi.core.users.factories as users_factories
 from pcapi.models import User
 
 
 @pytest.mark.usefixtures("db_session")
 class UserAutomationsTest:
-    @freeze_time("2021-08-01 02:00:00")
-    def test_get_users_who_turned_eighteen_today(self):
-        AGE18_ELIGIBLE_BIRTH_DATE = datetime.today() - relativedelta(years=ELIGIBILITY_AGE_18, months=0)
+    @freeze_time("2021-08-01 10:00:00")
+    def test_get_users_who_will_turn_eighteen_in_one_month(self):
         AGE20_NOT_ELIGIBLE_BIRTH_DATE = datetime.utcnow() - relativedelta(years=20)
+        _18_YEARS_IN_1_MONTHS = (
+            datetime.utcnow().replace(hour=3, minute=0, second=0, microsecond=0)
+            - relativedelta(years=ELIGIBILITY_AGE_18)
+            + relativedelta(days=30)
+        )
         user = users_factories.UserFactory(
-            email="fabien+test@example.net", firstName="Fabien", dateOfBirth=AGE18_ELIGIBLE_BIRTH_DATE
+            email="fabien+test@example.net", firstName="Fabien", dateOfBirth=_18_YEARS_IN_1_MONTHS
         )
         users_factories.UserFactory(
             email="bernard+test@example.net", firstName="Bernard", dateOfBirth=AGE20_NOT_ELIGIBLE_BIRTH_DATE
         )
-        result = get_users_who_turned_eighteen_today()
-        assert result[0].firstName == user.firstName == "Fabien"
-        assert result[0].age == user.age == 18
+        result = get_users_who_will_turn_eighteen_in_one_month()
         assert len(result) == 1
+        assert result[0].firstName == user.firstName
+        assert result[0].age == user.age
 
         assert len(User.query.all()) == 2
